4eae14d625f395402d1893cda83fdd08749a6bf5
Khushi <khushimattu10@gmail.com>
2022-05-24 11:32:03 +0530

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#FF6363',
+        secondary:{
+          100:'#E2E2D5',
+          200:'#888883'
+        }
+      }
+
+    },
+  },
+  plugins: [],
+}
